41c4f65839dc4304f7b905f58d317159751373f1
Miri <mirifurman@gmail.com>
2022-11-11 16:52:01 +0200

    dice working

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -1,14 +1,13 @@
 
-function RollDice(props){
-
-    
-
+function RollDice({roll1,roll2}){
 
     return(
-        <div className='dices'>      
-        <img src={`img${props.diceRoll1}`} alt="" width="100px"></img>
-        <img src={`img${props.diceRoll2}`} alt="" width="100px"></img> 
-        </div>
+
+    <div className='dices'>   
+     
+        <img src={`img/dice-${roll1}.png`} alt="" width="100px"></img>
+        <img src={`img/dice-${roll2}.png`} alt="" width="100px"></img> 
+    </div>
     )
 
     }
